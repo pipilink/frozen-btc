62745c8df09543982b3d9565649538d2f0f27979
[Eugene Martynov] <[dba@pipilink.net]>
2023-12-16 17:01:53 +0700

    load_blocks.py update

--- scripts/backend/load_blocks.py
+++ scripts/backend/load_blocks.py
@@ -5,14 +5,14 @@ from decoder import bitaddr
 
 load_dotenv()
 
-rpc_user=os.getenv('RPC_USER')
-rpc_password=os.getenv('RPC_PASSWORD')
+rpc_user = os.getenv('RPC_USER')
+rpc_password = os.getenv('RPC_PASSWORD')
 rpc_port = os.getenv('RPC_PORT')
 
 connection = cx_Oracle.connect(
-    user=os.getenv('ORACLE_USER'),
-    password=os.getenv('ORACLE_PASSWORD'),
-    dsn=os.getenv('ORACLE_DNS'))
+    user = os.getenv('ORACLE_USER'),
+    password = os.getenv('ORACLE_PASSWORD'),
+    dsn = os.getenv('ORACLE_DNS'))
 
 cursor = connection.cursor()
 cursor.execute('select max(block)+1 block_number from node')
@@ -24,40 +24,43 @@ def add_rec(block,datetime,address,trx,inout,value,move):
 
 rpc_connection = AuthServiceProxy(f'http://{rpc_user}:{rpc_password}@localhost:{rpc_port}')
 
-while True:
-    blockhash = rpc_connection.getblockhash(block_number)
-    block = rpc_connection.getblock(blockhash)
-    print('block #:', block['height'])
+try:
+    while True:
+        blockhash = rpc_connection.getblockhash(block_number)
+        block = rpc_connection.getblock(blockhash)
+        print('block #:', block['height'])
 
-    for txid in block['tx']:
-        raw_tx = rpc_connection.getrawtransaction(txid)
-        decoded_tx = rpc_connection.decoderawtransaction(raw_tx)
-        for vin_rec in decoded_tx['vin']:
-            if (vin_rec.get('coinbase') is None):
-                out_tx = rpc_connection.getrawtransaction(vin_rec.get('txid'))
-                dout_tx = rpc_connection.decoderawtransaction(out_tx)
-                rec_tx =dout_tx['vout'][int(vin_rec['vout'])]
+        for txid in block['tx']:
+            raw_tx = rpc_connection.getrawtransaction(txid)
+            decoded_tx = rpc_connection.decoderawtransaction(raw_tx)
+            for vin_rec in decoded_tx['vin']:
+                if (vin_rec.get('coinbase') is None):
+                    out_tx = rpc_connection.getrawtransaction(vin_rec.get('txid'))
+                    dout_tx = rpc_connection.decoderawtransaction(out_tx)
+                    rec_tx =dout_tx['vout'][int(vin_rec['vout'])]
 
-                if (float(rec_tx['value']) > 0):
-                    if( rec_tx['scriptPubKey']['type'] == 'pubkey'):
-                        addr = bitaddr(rec_tx['scriptPubKey']['asm'])
-                    elif (rec_tx['scriptPubKey']['type'] in ['multisig','nonstandard']):
+                    if (float(rec_tx['value']) > 0):
+                        if( rec_tx['scriptPubKey']['type'] == 'pubkey'):
+                            addr = bitaddr(rec_tx['scriptPubKey']['asm'])
+                        elif (rec_tx['scriptPubKey']['type'] in ['multisig','nonstandard']):
+                            addr = 'Unknown'
+                        else:
+                            addr = rec_tx['scriptPubKey']['address']
+
+                        add_rec(block['height'],block['time'],addr,vin_rec.get('txid'),'IN',rec_tx['value'],-1)
+
+            for vout_rec in decoded_tx['vout']:
+                if (float(vout_rec['value']) > 0):
+                    if( vout_rec['scriptPubKey']['type'] == 'pubkey'):
+                        addr = bitaddr(vout_rec['scriptPubKey']['asm'])
+                    elif (vout_rec['scriptPubKey']['type'] in ['multisig','nonstandard']):
                         addr = 'Unknown'
                     else:
-                        addr = rec_tx['scriptPubKey']['address']
+                        addr = vout_rec['scriptPubKey']['address']
 
-                    add_rec(block['height'],block['time'],addr,vin_rec.get('txid'),'IN',rec_tx['value'],-1)
+                    add_rec(block['height'],block['time'],addr,decoded_tx['txid'],'OUT',vout_rec['value'],1)
 
-        for vout_rec in decoded_tx['vout']:
-            if (float(vout_rec['value']) > 0):
-                if( vout_rec['scriptPubKey']['type'] == 'pubkey'):
-                    addr = bitaddr(vout_rec['scriptPubKey']['asm'])
-                elif (vout_rec['scriptPubKey']['type'] in ['multisig','nonstandard']):
-                    addr = 'Unknown'
-                else:
-                    addr = vout_rec['scriptPubKey']['address']
-
-                add_rec(block['height'],block['time'],addr,decoded_tx['txid'],'OUT',vout_rec['value'],1)
-
-        connection.commit()
-    block_number += 1
+            connection.commit()
+        block_number += 1
+except KeyboardInterrupt:
+    print('\nStop!')
